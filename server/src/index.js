10c4de3eea8b4f96e3de9b6d6222891feab6a486
Ntutu Sekonyela <n.sekonyela@alustudent.com>
2024-02-21 14:07:21 +0200

    made refreshers on routes, request and responses and middleware

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,39 @@ const app = express();
 
 const PORT = 3001;
 
+app.use(express.json());
+
+//when sending url encoded text you use this text
+//app.use(express.urlencoded())
+
 app.listen(PORT, () => console.log(`running express server at port: ${PORT}`));
 // app.listen(PORT, () => {
 //     return console.log(`App is listening to port: ${PORT}`);
 //   });
+
+const groceryList = [
+    {
+        item: "milk",
+        quantity: 2,
+      },
+      {
+        item: "cereal",
+        quantity: 1,
+      },
+      {
+        item: "milk",
+        quantity: 1,
+      },
+]
+
+app.get("/groceries", (request, response) => {
+  response.send(groceryList);
+});
+
+app.post("/groceries", (request, response) => {
+    console.log(request.body);
+    //the line below updates the grocery list so that when we request a new list, using a get request we get an updated list
+    groceryList.push(request.body);
+    response.send(201);
+})
+
