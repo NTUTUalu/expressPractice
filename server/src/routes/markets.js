f1fbd22f97e56f896f0061a9bb6f7ac759825359
Ntutu Sekonyela <n.sekonyela@alustudent.com>
2024-02-21 16:48:05 +0200

    implememnted query parameters

--- server/src/routes/markets.js
+++ server/src/routes/markets.js
@@ -4,20 +4,35 @@ const router = Router();
 const supermarkets = [
     
         {
+            id: 32,
+            miles: 14,
             store:"wholesaler 1"
         },
         {
+            id: 22,
+            miles: 12,
             store:"wholesaler 2"
         },
         {
+            id: 112,
+            miles: 3,
             store:"wholesaler 3"
         },
         {
+            id: 12,
+            miles: 1,
             store:"wholesaler 4"
         },
     
 ]
 router.get('/supermarkets', (request,response) => {
+    // console.log(request.query),
+    const {miles} = request.query;
+    const parsedMiles = parseInt(miles);
+    if(!isNaN(parsedMiles)) {
+        const filteredStores = supermarkets.filter((s) => s.miles <= parsedMiles);
+        response.send(filteredStores);
+    }else 
     response.send(supermarkets);
 })
 
