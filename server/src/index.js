a4407c80fcef4dfa450b04f937b0b03116bf8ba9
Ntutu Sekonyela <n.sekonyela@alustudent.com>
2024-02-22 18:04:11 +0200

    cookies did not work

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const cookieParser = require('cookie-parser')
 // import express from "express";
+const session = require('express-session');
 const groceriesRoute = require("./routes/groceries");
 const marketsRoute = require('./routes/markets');
 const app = express();
@@ -11,11 +12,22 @@ const PORT = 3001;
 app.use(express.json());
 // app.use(groceriesRoute);
 //when sending url encoded text you use this text
-//app.use(express.urlencoded())
+app.use(express.urlencoded())
 
-app.use('/api', groceriesRoute);
-app.use('/api', marketsRoute);
+app.use('/api/groceries', groceriesRoute);
+app.use('/api/v1/markets', marketsRoute);
 app.use(cookieParser());
+
+app.use((req,res,next) => {
+    console.log(`$(req.method):$(req.url)`);
+    next();
+})
+
+app.use(session({
+    secret: "JVLUVBEVERUEIBCIERBERBVIVBEIVBEUVB",
+    resave: false,
+    saveUninitialized: false,
+}))
 
 app.listen(PORT, () => console.log(`running express server at port: ${PORT}`));
 // app.listen(PORT, () => {
--- server/src/routes/groceries.js
+++ server/src/routes/groceries.js
@@ -18,32 +18,60 @@ const groceryList = [
       },
 ]
 
-router.get("/", (request, response) => {
-  response.cookie('visited', true, {
-    maxAge:10000,
-  });
-});
+// router.get("/", (request, response) => {
+//   response.cookie('visited', true, {
+//     maxAge:10000,
+//   });
+// });
 
-router.get("/groceries", (request, response) => {
+router.get("/", (request, response) => {
   response.cookie('visited', true, {
     maxAge:10000,
   });
   response.send(groceryList);
 });
 
-router.get('/groceries/:item', (request,response) => {
-    // console.log(request.params.item);
-    console.log(request.cookies)
+router.get('/:item', (request,response) => {
+    console.log(request.cookies);
+    // console.log(request.cookies)
     const {item} = request.params;
     const groceryItem = groceryList.find((g) => g.item === item)
     response.send(groceryItem);
 })
 
-router.post("/groceries", (request, response) => {
+router.post("/", (request, response) => {
     console.log(request.body);
     //the line below updates the grocery list so that when we request a new list, using a get request we get an updated list
     groceryList.push(request.body);
     response.send(201);
-})
+});
+
+// router.get('/shopping/cart', (request, response) => {
+//  const {cart} = request.session;
+
+//  if(!cart) {
+//   response.send('you have no cart session');
+//  }else {
+//   response.send(cart);
+//  }
+// })
+
+// router.post('/shopping/cart/item', (request, response) => {
+//   const {item, quantity} = request.body;
+//   const cartItem = {item, quantity};
+
+//   // console.log(cartItem);
+//   // console.log(request.session)
+//   // the problm with the session is that cart is undefined we cannot desrtucture it
+//   const {cart} = request.session;
+//   if(cart) {
+//     request.session.cart.items.push(cartItem);
+//   }else {
+//     request.session.cart ={
+//       items: [cartItem]
+//     }
+//   }
+//   response.send(201)
+// })
 
 module.exports = router; 
